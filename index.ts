5e78e5a0443d3a2b8d2ce02e7ca5f89408715f73
Pragati Agarwal <pragatiagarwal121@gmail.com>
2020-11-24 21:38:31 +0530

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,24 @@
+/* document.addEventListener('click', () => {
+console.log("Clicked")})
+*/
+//import { Observable } from 'rxjs'
+
+/* import { fromEvent } from "rxjs";
+import { scan } from "rxjs/operators";
+
+fromEvent(document, "click")
+  .pipe(scan(count => count + 1, 0)) //keeps the data integrity as nobody can modify it except subscribe!!
+  .subscribe(count => console.log(` Clicked at: ${count}`)); */
+
+  import { Observable } from 'rxjs'
+
+  const observable = new Observable(subscriber => {
+    subscriber.next(1);
+    subscriber.next(2);
+    subscriber.next(3);
+
+    setTimeout( () => {
+        subscriber.next(4);
+        subscriber.complete();  //like return statement subscriber ends observing 
+    }, 2000 )
+  });
